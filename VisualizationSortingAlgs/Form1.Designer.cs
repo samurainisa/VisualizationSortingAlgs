fdfe52790f9c750fb01f8f42ab907452960c8bcf
workspace <sadovikov.ne@kemerovorea.ru>
2021-10-28 20:17:54 +0700

    1

--- VisualizationSortingAlgs/Form1.Designer.cs
+++ VisualizationSortingAlgs/Form1.Designer.cs
@@ -29,13 +29,160 @@ namespace VisualizationSortingAlgs
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Mass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genBtn = new System.Windows.Forms.Button();
+            this.arraynum = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.bubblecheck = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Mass});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(243, 516);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Mass
+            // 
+            this.Mass.HeaderText = "Массив";
+            this.Mass.Name = "Mass";
+            this.Mass.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Mass.Width = 200;
+            // 
+            // genBtn
+            // 
+            this.genBtn.Location = new System.Drawing.Point(69, 575);
+            this.genBtn.Name = "genBtn";
+            this.genBtn.Size = new System.Drawing.Size(106, 41);
+            this.genBtn.TabIndex = 1;
+            this.genBtn.Text = "Сгенерировать массив";
+            this.genBtn.UseVisualStyleBackColor = true;
+            this.genBtn.Click += new System.EventHandler(this.genBtn_Click);
+            // 
+            // arraynum
+            // 
+            this.arraynum.Location = new System.Drawing.Point(69, 549);
+            this.arraynum.Name = "arraynum";
+            this.arraynum.Size = new System.Drawing.Size(106, 20);
+            this.arraynum.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закрытьToolStripMenuItem,
+            this.excelToolStripMenuItem,
+            this.googleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1394, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.excelToolStripMenuItem.Text = "Excel";
+            this.excelToolStripMenuItem.Click += new System.EventHandler(this.excelToolStripMenuItem_Click);
+            // 
+            // googleToolStripMenuItem
+            // 
+            this.googleToolStripMenuItem.Name = "googleToolStripMenuItem";
+            this.googleToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.googleToolStripMenuItem.Text = "Google";
+            this.googleToolStripMenuItem.Click += new System.EventHandler(this.googleToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(782, 43);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(554, 443);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // bubblecheck
+            // 
+            this.bubblecheck.AutoSize = true;
+            this.bubblecheck.Location = new System.Drawing.Point(299, 43);
+            this.bubblecheck.Name = "bubblecheck";
+            this.bubblecheck.Size = new System.Drawing.Size(78, 17);
+            this.bubblecheck.TabIndex = 5;
+            this.bubblecheck.Text = "BubbleSort";
+            this.bubblecheck.UseVisualStyleBackColor = true;
+            this.bubblecheck.CheckedChanged += new System.EventHandler(this.bubblecheck_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1394, 725);
+            this.Controls.Add(this.bubblecheck);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.arraynum);
+            this.Controls.Add(this.genBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button genBtn;
+        private System.Windows.Forms.TextBox arraynum;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem googleToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mass;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckBox bubblecheck;
     }
 }
 
--- VisualizationSortingAlgs/Form1.cs
+++ VisualizationSortingAlgs/Form1.cs
@@ -1,20 +1,163 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Microsoft.Office.Interop.Excel;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
 
 namespace VisualizationSortingAlgs
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #region парсинг данных и генерация
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = String.Empty;
+            DialogResult res = openFileDialog1.ShowDialog();
+            if (res != DialogResult.OK) return;
+
+            try
+            {
+                dataGridView1.Rows.Clear();
+                Application ObjWorkExcel = new Application();
+                Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(openFileDialog1.FileName, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing); //открыть файл
+                Worksheet ObjWorkSheet = (Worksheet)ObjWorkBook.Sheets[1];
+
+                var lastCell = ObjWorkSheet.Cells.SpecialCells(GetXlCellTypeLastCell());
+                int lastColumn = lastCell.Column;
+                int lastRow = lastCell.Row;
+
+                if (ObjWorkSheet.Rows.CurrentRegion.EntireRow.Count == 1)
+                {
+                    MessageBox.Show("No data found.");
+                }
+                else
+                {
+                    string sx = String.Empty;
+
+                    for (int i = 0; i < lastCell.Row; i++)
+                    {
+                        sx = ObjWorkSheet.Cells[i + 1, 1].Text.ToString();
+                        if (sx.Trim() != String.Empty)
+                        {
+                            dataGridView1.Rows.Add(sx);
+                        }
+                    }
+                    GC.Collect();
+                }
+
+                ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+                ObjWorkExcel.Quit();
+                ObjWorkExcel.Quit();
+                GC.Collect();
+            }
+
+            catch (Exception exception)
+            {
+                MessageBox.Show($"При попытке загрузки из Excel произошла обшика!\n{exception.Message}", "Error!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private static XlCellType GetXlCellTypeLastCell()
+        {
+            return XlCellType.xlCellTypeLastCell;
+        }
+
+
+        private static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        private const string GoogleCredentialsFileName = @"C:\Users\workspace.DESKTOP-N5S6IG3\source\repos\VisualizationSortingAlgs\VisualizationSortingAlgs\clients_secrets.json";
+        private const string ReadRange = "Лист1!A:A";
+        private const string SpreadsheetId = "1GoOUEb2OdQWLqPIJbEB_wMnfy4sJwc4cSkdMra2AEKM";
+
+        private async void googleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            await readAsync();
+        }
+        async Task readAsync()
+        {
+            var serviceValues = GetSheetsService().Spreadsheets.Values;
+            await ReadAsync(serviceValues);
+        }
+
+        private static SheetsService GetSheetsService()
+        {
+            using (var stream = new FileStream(GoogleCredentialsFileName, FileMode.Open, FileAccess.Read))
+            {
+                var serviceInitializer = new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = GoogleCredential.FromStream(stream).CreateScoped(Scopes)
+                };
+                return new SheetsService(serviceInitializer);
+            }
+        }
+
+        private  async Task ReadAsync(SpreadsheetsResource.ValuesResource valuesResource)
+        {
+            var response = await valuesResource.Get(SpreadsheetId, ReadRange).ExecuteAsync();
+            var values = response.Values;
+
+            if (values == null || !values.Any())
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+
+            else
+            {
+                foreach (var row in values.Skip(1))
+                {
+                    var res = string.Join(" ", row.Select(r => r.ToString()));
+                    dataGridView1.Rows.Add(res);
+                }
+            }
+        }
+
+
+        private void genBtn_Click(object sender, EventArgs e)
+        {
+            GenerateData();
+        }
+
+        public void GenerateData()
+        {
+                dataGridView1.AllowUserToAddRows = false;
+                dataGridView1.Rows.Clear();
+                int Yn = Convert.ToInt32(arraynum.Text);
+                Random rnd = new Random();
+
+                for (int row = 0; row < Yn; row++)
+                {
+                    dataGridView1.Rows.Add();
+                    dataGridView1[0, row].Value = rnd.Next(100);
+                }
+                dataGridView1.AllowUserToAddRows = true;
+        }
+
+
+        #endregion
+
+        private void bubblecheck_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
